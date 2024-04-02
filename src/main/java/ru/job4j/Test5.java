6df05425102d2095bf31c4ca76f13eefa568348e
shef72 <nikola-panin@yandex.ru>
2024-04-02 10:47:15 +0300

    4.1. Разбор первой программы. [#159033 #28073] сделал

---
+++ src/main/java/ru/job4j/Test5.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Test5 {
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2");
+       System.out.println("2 * 2 = 4");
+       System.out.println("3 * 5 = 6");
+       System.out.println("4 * 2 = 8");
+       System.out.println("5 * 2 = 10");
+       System.out.println("6 * 2 = 12");
+       System.out.println("7 * 2 = 14");
+       System.out.println("8 * 2 = 16");
+       System.out.println("9 * 2 = 18");
+
+    }
+}
