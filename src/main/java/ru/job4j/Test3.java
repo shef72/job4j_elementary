4f0b5fc8ac716974a83c4bf8b3f9b1ffd628cad5
shef72 <nikola-panin@yandex.ru>
2024-04-03 17:39:51 +0300

    начало

---
+++ src/main/java/ru/job4j/Test3.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Test3 {
+    public static void main(String[] args) {
+       int nom = 5;
+        System.out.println(nom);
+        byte nom1 = 7;
+        System.out.println(nom1);
+    }
+}
