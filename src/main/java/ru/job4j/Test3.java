7193bc1d87dac6f3730ee9298ce27cf3fd1abc6d
shef72 <nikola-panin@yandex.ru>
2024-04-03 17:43:27 +0300

    начало + 1;

--- src/main/java/ru/job4j/Test3.java
+++ src/main/java/ru/job4j/Test3.java
@@ -4,7 +4,7 @@ public class Test3 {
     public static void main(String[] args) {
        int nom = 5;
         System.out.println(nom);
-        byte nom1 = 7;
+        byte nom1 = 9;
         System.out.println(nom1);
     }
 }
