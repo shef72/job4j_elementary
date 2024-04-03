3d1c96ccef90c40016409ef2438b7deafedb4e71
shef72 <nikola-panin@yandex.ru>
2024-04-03 20:40:32 +0300

    0. Что такое переменная. [#3610 #28061] первый урок.

--- src/main/java/ru/job4j/Test2.java
+++ src/main/java/ru/job4j/Test2.java
@@ -2,6 +2,22 @@ package ru.job4j;
 
 public class Test2 {
     public static void main(String[] args) {
+        byte age = 52;
+        System.out.println(age);
+        char men = 'M';
+        System.out.println(men);
+        float height = 1.75f;
+        System.out.println(height);
+        short pieces = 30000;
+        System.out.println(pieces);
+        int bacteria = 2050478321;
+        System.out.println(bacteria);
+        long space = 812345678901234567l;
+        System.out.println(space);
+        boolean work = true;
+        System.out.println(work);
+        byte memori = 99;
+        System.out.println(memori);
 
     }
 }
