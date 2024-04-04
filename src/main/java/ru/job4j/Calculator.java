86d3602485ef4b7f83c144b850833905afd73969
shef72 <nikola-panin@yandex.ru>
2024-04-04 18:28:40 +0300

    3. Арифметические операции. [#185 #28064]

---
+++ src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int plus = four + six;
+        System.out.println(plus);
+    }
+}
