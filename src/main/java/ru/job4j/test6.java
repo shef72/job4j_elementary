d74969b2bcf0262a7ee638e43dec25a9e42f2dac
shef72 <nikola-panin@yandex.ru>
2024-04-04 17:56:05 +0300

    2. Переменные. [#172635 #28052]

---
+++ src/main/java/ru/job4j/test6.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class test6 {
+    public static void main(String[] args) {
+        byte age = 51;
+        System.out.println(age);
+        char men = 'M';
+        System.out.println(men);
+        short height = 174;
+        System.out.println(height);
+        int pieces = 31500;
+        System.out.println(pieces);
+        long bactery = 3200000000l;
+        System.out.println(bactery);
+        double cosmos = 160000000.000000;
+        System.out.println(cosmos);
+        boolean work = false;
+        System.out.println(work);
+        float memory = 2.2f;
+        System.out.println(memory);
+
+    }
+
+
+}
