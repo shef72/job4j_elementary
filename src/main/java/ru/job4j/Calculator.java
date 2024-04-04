f21fc159020c3d3303bfa75ae9a934f27fdd35d4
shef72 <nikola-panin@yandex.ru>
2024-04-04 18:57:33 +0300

    3. Арифметические операции. [#185 #28064]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -2,10 +2,18 @@ package ru.job4j;
 
 public class Calculator {
     public static void main(String[] args) {
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int plus = four + six;
-        System.out.println(plus);
+       // int six = 6;
+       // int four = 4;
+       // int five = 5;
+       // int plus = four + six;
+       // System.out.println(plus);
+
+
+            int six = 80;
+            int div = 20;
+            int two = 10;
+            int division = six / two;
+            System.out.println(division);
+        }
     }
-}
+
