cecf05aeef038b8cb24d4ae35ffbb6615ad93412
shef72 <nikola-panin@yandex.ru>
2024-04-08 17:21:24 +0300

    4. Методы [#231984 #28058]

--- src/main/java/ru/job4j/ArgumentsMethod.java
+++ src/main/java/ru/job4j/ArgumentsMethod.java
@@ -1,19 +1,19 @@
 package ru.job4j;
 
 public class ArgumentsMethod {
-    public static void hello(String name, int age) {
+    public static void hello(int age, String name) {
         System.out.println("Hello, " + name + ", age = " + age);
 
     }
 
     public static void main(String[] args) {
         String name = "Job4j";
-        String age = "6";
+        int age = 6;
 
-        ArgumentsMethod.hello("Job4j", 6);
-        ArgumentsMethod.hello("Job4j", 6);
-        ArgumentsMethod.hello("Job4j", 6);
-        ArgumentsMethod.hello("Job4j", 6);
+        ArgumentsMethod.hello(age, name);
+      //  ArgumentsMethod.hello("Job4j", 6);
+      //  ArgumentsMethod.hello("Job4j", 6);
+      //  ArgumentsMethod.hello("Job4j", 6);
 
 
     }
