0c90168b9ff79ad9ba7706dbafe09ff92806fc5c
shef72 <nikola-panin@yandex.ru>
2024-04-10 18:41:56 +0300

    4.2. Результат работы метода [#232622 #28063]
    5. Конвертер валюты [#41583 #28057]
    
    + 1

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = -1; /* формула перевода рублей в доллары. */
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+    }
+}
