f002a59d994b63fa3af4750c38d12dfae7eae275
shef72 <nikola-panin@yandex.ru>
2024-04-17 00:21:25 +0300

    4.1.2. Глупый бот [#31323 #27994]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String result = "I don't know. Please, ask another question.";
+        if ("Hi, Bot.".equals(question)) {
+            /* заменить ... на правильный ответ result = "ответ по заданию". */
+            result = "Hi, Bot.";
+        } else if ("Bye.".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
+            /* заменить ... на правильный ответ result = "ответ по заданию". */
+            result = "Bye.";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyBot.answer("Hi, Bot.");
+        System.out.println(result);
+        result = DummyBot.answer("Bye.");
+        System.out.println(result);
+    }
+}
