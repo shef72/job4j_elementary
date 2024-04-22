f4078b516dcdf42b0ece18df1c33ac41590c54e9
shef72 <nikola-panin@yandex.ru>
2024-04-23 00:35:53 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #28041] + 1

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -3,6 +3,16 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
+        while (ivan <= nik) {
+            ivan = ivan * 3;
+            nik = nik * 2;
+            month++;
+        }
         return month;
+    }
+
+    public static void main(String[] args) {
+        int num = calc(90, 100);
+        System.out.println(num);
     }
 }
